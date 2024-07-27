23c1bea1dd625961746f51462f7efced79bc62d1
Mohamed Elbeltagy <mohamed.elbeltagy@nortal.com>
2024-07-27 18:32:29 +0300

    refactor: Move lister's configurations into own common configuration package
    Refs: XRDCAT-8

--- xroad-catalog-lister/src/main/java/org/niis/xroad/catalog/lister/configuration/WebServiceConfig.java
+++ xroad-catalog-lister/src/main/java/org/niis/xroad/catalog/lister/configuration/WebServiceConfig.java
@@ -10,8 +10,10 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package fi.vrk.xroad.catalog.lister;
+package org.niis.xroad.catalog.lister.configuration;
 
+import org.niis.xroad.catalog.lister.interceptor.ServiceEndpointInterceptor;
+import org.niis.xroad.catalog.lister.interceptor.WsdlCdataInterceptor;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
