e489b779fcab73db7d2b0e1fa4a478f1c33dc625
Mohamed Elbeltagy <mohamed.elbeltagy@nortal.com>
2024-07-30 09:30:38 +0300

    refactor: Undo moving repository tests to lister service (moved by mistake)
    Refs: XRDCAT-8

--- xroad-catalog-persistence/src/test/java/org/niis/xroad/catalog/persistence/repository/ErrorLogRepositoryTest.java
+++ xroad-catalog-persistence/src/test/java/org/niis/xroad/catalog/persistence/repository/ErrorLogRepositoryTest.java
@@ -10,10 +10,9 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package fi.vrk.xroad.catalog.persistence;
+package org.niis.xroad.catalog.persistence.repository;
 
-import fi.vrk.xroad.catalog.persistence.entity.ErrorLog;
-import fi.vrk.xroad.catalog.persistence.repository.ErrorLogRepository;
+import org.niis.xroad.catalog.persistence.entity.ErrorLog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
