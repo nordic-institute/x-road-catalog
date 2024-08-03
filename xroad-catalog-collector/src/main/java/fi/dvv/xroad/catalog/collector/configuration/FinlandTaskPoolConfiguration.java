750de0fa0e550635a876093d1db437f2d5a506b0
Mohamed Elbeltagy <mohamed.elbeltagy@nortal.com>
2024-08-03 10:00:30 +0300

    refactor: Group Finland specific properties into a nested properties under xroad-catalog.country.fi.*, and update docs
    Refs: XRDCAT-8

--- xroad-catalog-collector/src/main/java/fi/dvv/xroad/catalog/collector/configuration/FinlandTaskPoolConfiguration.java
+++ xroad-catalog-collector/src/main/java/fi/dvv/xroad/catalog/collector/configuration/FinlandTaskPoolConfiguration.java
@@ -36,33 +36,33 @@ public class FinlandTaskPoolConfiguration {
 
     // Parameters related to the "fi" profile
 
-    @Value("${xroad-catalog.fetch-organizations-url}")
-    private String fetchOrganizationsUrl;
+    @Value("${xroad-catalog.country.fi.fetch.companies.pool-size:10}")
+    private int fetchCompaniesPoolSize;
 
-    @Value("${xroad-catalog.fetch-companies-url}")
+    @Value("${xroad-catalog.country.fi.fetch.companies.url}")
     private String fetchCompaniesUrl;
 
-    @Value("${xroad-catalog.fetch-external-limit:500}")
-    private int fetchExternalLimit;
-
-    @Value("${xroad-catalog.fetch-external-update-after-days:7}")
-    private int fetchExternalUpdateAfterDays;
-
-    @Value("${xroad-catalog.fetch-external-interval-min:20}")
-    private long fetchExternalInterval;
-
-    @Value("${xroad-catalog.fetch-external-run-unlimited:false}")
-    private boolean fetchExternalRunUnlimited;
-
-    @Value("${xroad-catalog.fetch-external-time-after-hour:3}")
-    private int fetchExternalTimeAfterHour;
-
-    @Value("${xroad-catalog.fetch-external-time-before-hour:4}")
-    private int fetchExternalTimeBeforeHour;
-
-    @Value("${xroad-catalog.fetch-organizations-pool-size:10}")
+    @Value("${xroad-catalog.country.fi.fetch.organizations.pool-size:10}")
     private int fetchOrganizationsPoolSize;
 
-    @Value("${xroad-catalog.fetch-companies-pool-size:10}")
-    private int fetchCompaniesPoolSize;
+    @Value("${xroad-catalog.country.fi.fetch.organizations.url}")
+    private String fetchOrganizationsUrl;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-limit:500}")
+    private int fetchExternalLimit;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-update-after-days:7}")
+    private int fetchExternalUpdateAfterDays;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-interval-min:20}")
+    private long fetchExternalInterval;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-run-unlimited:false}")
+    private boolean fetchExternalRunUnlimited;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-time-after-hour:3}")
+    private int fetchExternalTimeAfterHour;
+
+    @Value("${xroad-catalog.country.fi.fetch.external-time-before-hour:4}")
+    private int fetchExternalTimeBeforeHour;
 }
