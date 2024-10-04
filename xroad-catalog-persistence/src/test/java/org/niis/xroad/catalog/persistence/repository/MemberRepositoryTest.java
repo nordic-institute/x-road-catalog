86679bfae9dedf93e6ce6ea046f8a2f060a6d2b9
Mohamed ElBeltagy <60988715+melbeltagy@users.noreply.github.com>
2024-10-04 15:35:49 +0300

    chore: Update license headers' formatting to fix pmd CommentSize rule and configure CommentSize rule to fix pmd issues (#67)

--- xroad-catalog-persistence/src/test/java/org/niis/xroad/catalog/persistence/repository/MemberRepositoryTest.java
+++ xroad-catalog-persistence/src/test/java/org/niis/xroad/catalog/persistence/repository/MemberRepositoryTest.java
@@ -4,11 +4,23 @@
  * Copyright (c) 2023- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2016-2023 Finnish Digital Agency
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.niis.xroad.catalog.persistence.repository;
 
@@ -29,7 +41,6 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -83,75 +94,75 @@ public class MemberRepositoryTest {
         assertEquals(7, Iterables.size(members));
     }
 
-/*    @Test
-    public void testAddMember() {
-        String name = "memberx";
-        Member member = createTestMember(name);
-        memberRepository.save(member);
-
-        Member peek = memberRepository.findActiveByNaturalKey(
-                member.getXRoadInstance(),
-                member.getMemberClass(),
-                member.getMemberCode());
-        assertNotNull(peek);
-        assertNotNull(peek.getStatusInfo());
-
-        Iterable<Member> members = memberRepository.findAllActive();
-        assertEquals(8, Iterables.size(members));
-        log.info("created member with id=" + member.getId());
-
-        testUtil.entityManagerFlush();
-        testUtil.entityManagerClear();
-
-        Member savedRead = memberRepository.findActiveByNaturalKey(
-                member.getXRoadInstance(),
-                member.getMemberClass(),
-                member.getMemberCode());
-        assertNotNull(savedRead);
-        assertEquals(2, savedRead.getActiveSubsystems().size());
-    }*/
-
-    private Member createTestMember(String name) {
-        Member member = new Member();
-        member.setName(name);
-        member.setXRoadInstance("xroadinstance-" + name);
-        member.setMemberClass("mclass-" + name);
-        member.setMemberCode("mcode-" + name);
-        member.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-
-        Subsystem ss1 = new Subsystem();
-        Subsystem ss2 = new Subsystem();
-        ss1.setSubsystemCode(name + "ss1");
-        ss2.setSubsystemCode(name + "ss2");
-        ss1.setMember(member);
-        ss2.setMember(member);
-        ss1.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-        ss2.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-        member.setSubsystems(new HashSet<>());
-        member.getAllSubsystems().add(ss1);
-        member.getAllSubsystems().add(ss2);
-
-        Service s1 = new Service();
-        Service s2 = new Service();
-        s1.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-        s2.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-        s1.setSubsystem(ss1);
-        s2.setSubsystem(ss1);
-        ss2.setServices(new HashSet<>());
-        ss2.getAllServices().add(s1);
-        ss2.getAllServices().add(s2);
-        s1.setServiceCode(name + "service1");
-        s2.setServiceCode(name + "service2");
-
-        Wsdl wsdl = new Wsdl();
-        wsdl.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
-        s1.setWsdl(wsdl);
-        wsdl.setService(s1);
-        wsdl.initializeExternalId();
-        wsdl.setData("<?xml version=\"1.0\" standalone=\"no\"?><wsdl/>");
-
-        return member;
-    }
+//    @Test
+//    public void testAddMember() {
+//        String name = "memberx";
+//        Member member = createTestMember(name);
+//        memberRepository.save(member);
+//
+//        Member peek = memberRepository.findActiveByNaturalKey(
+//                member.getXRoadInstance(),
+//                member.getMemberClass(),
+//                member.getMemberCode());
+//        assertNotNull(peek);
+//        assertNotNull(peek.getStatusInfo());
+//
+//        Iterable<Member> members = memberRepository.findAllActive();
+//        assertEquals(8, Iterables.size(members));
+//        log.info("created member with id=" + member.getId());
+//
+//        testUtil.entityManagerFlush();
+//        testUtil.entityManagerClear();
+//
+//        Member savedRead = memberRepository.findActiveByNaturalKey(
+//                member.getXRoadInstance(),
+//                member.getMemberClass(),
+//                member.getMemberCode());
+//        assertNotNull(savedRead);
+//        assertEquals(2, savedRead.getActiveSubsystems().size());
+//    }
+//
+//    private Member createTestMember(String name) {
+//        Member member = new Member();
+//        member.setName(name);
+//        member.setXRoadInstance("xroadinstance-" + name);
+//        member.setMemberClass("mclass-" + name);
+//        member.setMemberCode("mcode-" + name);
+//        member.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//
+//        Subsystem ss1 = new Subsystem();
+//        Subsystem ss2 = new Subsystem();
+//        ss1.setSubsystemCode(name + "ss1");
+//        ss2.setSubsystemCode(name + "ss2");
+//        ss1.setMember(member);
+//        ss2.setMember(member);
+//        ss1.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//        ss2.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//        member.setSubsystems(new HashSet<>());
+//        member.getAllSubsystems().add(ss1);
+//        member.getAllSubsystems().add(ss2);
+//
+//        Service s1 = new Service();
+//        Service s2 = new Service();
+//        s1.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//        s2.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//        s1.setSubsystem(ss1);
+//        s2.setSubsystem(ss1);
+//        ss2.setServices(new HashSet<>());
+//        ss2.getAllServices().add(s1);
+//        ss2.getAllServices().add(s2);
+//        s1.setServiceCode(name + "service1");
+//        s2.setServiceCode(name + "service2");
+//
+//        Wsdl wsdl = new Wsdl();
+//        wsdl.getStatusInfo().setTimestampsForNew(LocalDateTime.now());
+//        s1.setWsdl(wsdl);
+//        wsdl.setService(s1);
+//        wsdl.initializeExternalId();
+//        wsdl.setData("<?xml version=\"1.0\" standalone=\"no\"?><wsdl/>");
+//
+//        return member;
+//    }
 
     @Test
     public void testGetChangedSince() {
